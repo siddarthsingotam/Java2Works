19813f1445fb693df5dd25c63a0b04112da8acdb
Siddarth <siddarthsingotam1213@gmail.com>
2023-12-21 00:59:48 +0530

    Exercise 8.2.1 completed

---
+++ Module8.2/src/main/java/NumberOperations.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class NumberOperations {
+
+    public static void main(String[] args) {
+        // Example list of integers
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        // Perform operations using functional programming
+        int sum = numbers.stream()
+                .filter(NumberOperations::isOdd)    // Filter out even numbers
+                .map(NumberOperations::doubleNumber) // Double each remaining number
+                .reduce(0, Integer::sum);            // Find the sum
+
+        // Print the result
+        System.out.println("Sum of doubled odd numbers: " + sum);
+    }
+
+    // Predicate to check if a number is odd
+    private static boolean isOdd(int number) {
+        return number % 2 != 0;
+    }
+
+    // Function to double a number
+    private static int doubleNumber(int number) {
+        return number * 2;
+    }
+}
+
