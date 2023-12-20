294366bb74fed70c3cc13652162917e8fb2c4a1f
Siddarth <siddarthsingotam1213@gmail.com>
2023-12-20 23:39:01 +0530

    Exercise 6.3.1 updated

--- Module6.3/src/main/java/Main.java
+++ Module6.3/src/main/java/Main.java
@@ -8,23 +8,23 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        // Step 1: Create a Pet object with an initial location
+        //Create a Pet object with an initial location
         Pet pet = new Pet(new Point2D(0, 0));
 
-        // Step 2: Create a PetView object with a specified canvas size and pet image path
+        //Create a PetView object with a specified canvas size and pet image path
         PetView petView = new PetView(800, 600, "path/to/pet/image.png");
 
-        // Step 3: Create a PetController object with the Pet and PetView objects
+        // PetController object with the Pet and PetView objects
         PetController petController = new PetController(pet, petView);
 
-        // Step 4: Set up a Scene with the PetView's canvas
+        //Set up a Scene with the PetView's canvas
         Scene scene = new Scene(petView.getCanvas().getParent());
 
-        // Step 5: Set up mouse event handlers for the Scene that call the PetController's methods
+        //mouse event handlers for the Scene that call the PetController's methods
         scene.setOnMouseMoved(event -> petController.handleMouseMoved(event));
         scene.setOnMouseExited(event -> petController.handleMouseExited());
 
-        // Step 6: Create a Stage and show the Scene
+        //Stage and show the Scene
         primaryStage.setScene(scene);
         primaryStage.setTitle("Virtual Pet");
         primaryStage.show();
